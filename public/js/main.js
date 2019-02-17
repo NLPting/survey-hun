13f48bbf39337bd6391e7d4e76bf7f7514cd1cd3
蔡仲庭 <caizhongting@caizhongtingde-MacBook-Pro.local>
2019-02-17 13:54:24 +0800

    version2

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,12 @@
 // Turn on Bootstrap
-$('[data-toggle="tooltip"]').tooltip();
+
 $('#survery').hide()
+$('#alert-show').hide()
+$('#alert-show1').hide()
     
 // 15 days from now!
 function get15dayFromNow() {
-  return new Date(new Date().valueOf() + 60 * 1100);
+  return new Date(new Date().valueOf() + 60 * 1160);
 }
 
 var $clock = $('#clock');
@@ -19,9 +21,11 @@ $('#btn-reset').click(function() {
     //$(this).html(event.strftime('%D days %H:%M:%S'));
     $(this).html(event.strftime('%M:%S'));
     $('#btn-reset').hide()
-    $('.content').hide()
+    $('#btn-r').css('padding',0);
+    $('#content').hide()
+    $('#card-1').css('top',10);
     $('#survery').show()
-    console.log(event.offset.minutes , event.offset.seconds)
+    //console.log(event.offset.minutes , event.offset.seconds)
 
     alert_reminder(event.offset.minutes , event.offset.seconds)
 
@@ -30,9 +34,18 @@ $('#btn-reset').click(function() {
 $('.alert_row').hide()
 function alert_reminder(minutes , seconds){
     if (minutes==1 && seconds==0){
-        $('.alert_row').show();
+        $('#alert-show').css('top',15);
+        $('#alert-show').show()
+        $('#alert-show1').css('top',15);
+        $('#alert-show1').show()
+        shake()
     }
+}
 
+function shake(){
+    setInterval(function(){ 
+        $(".alert-card").hide();
+    }, 3000);
 }
 
 
