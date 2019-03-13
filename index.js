040306e2443ef336f4a263be4727ea4d19aa4518
蔡仲庭 <ting@caizhongtingde-MacBook-Pro.local>
2019-03-13 20:33:40 +0800

    version2

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ app.get('/', function(req, res){
 
 
 
-http.listen(3000, function(){
+http.listen(3001, function(){
   console.log('listening on *:3000');
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,52 +1,50 @@
 // Turn on Bootstrap
-
-$('#survery').hide()
-$('#alert-show').hide()
-$('#alert-show1').hide()
     
 // 15 days from now!
 function get15dayFromNow() {
   return new Date(new Date().valueOf() + 60 * 1160);
 }
 
-var $clock = $('#clock');
+var $clock = $('.clock');
 
 
 
 
 
-$('#btn-reset').click(function() {
+$('#go').click(function() {
   //$clock.countdown(get15dayFromNow());
-  $clock.countdown(get15dayFromNow(), function(event) {
-    //$(this).html(event.strftime('%D days %H:%M:%S'));
-    $(this).html(event.strftime('%M:%S'));
-    $('#btn-reset').hide()
-    $('#btn-r').css('padding',0);
-    $('#content').hide()
-    $('#card-1').css('top',10);
-    $('#survery').show()
-    //console.log(event.offset.minutes , event.offset.seconds)
+  $('.page1').hide()
+  document.getElementById('surveycake').innerHTML = "<iframe width='100%' height='800' src='https://www.surveycake.com/s/NeQgy' style='overflow:hidden;border:#ddd 1px solid;' allowTransparency='true' frameborder='0'> </iframe>"
+  setTimeout(function(){
 
-    alert_reminder(event.offset.minutes , event.offset.seconds)
+    $clock.countdown(get15dayFromNow(), function(event) {
+      //$(this).html(event.strftime('%D days %H:%M:%S'));
+      $(this).html(event.strftime('%M:%S'));
+      
+      //console.log(event.offset.minutes , event.offset.seconds)
+  
+      //alert_reminder(event.offset.minutes , event.offset.seconds)
+  
+    });
 
-  });
+   }, 5000);
 });
-$('.alert_row').hide()
-function alert_reminder(minutes , seconds){
-    if (minutes==1 && seconds==0){
-        $('#alert-show').css('top',15);
-        $('#alert-show').show()
-        $('#alert-show1').css('top',15);
-        $('#alert-show1').show()
-        shake()
-    }
-}
+//$('.alert_row').hide()
+//function alert_reminder(minutes , seconds){
+//    if (minutes==1 && seconds==0){
+//        $('#alert-show').css('top',15);
+//        $('#alert-show').show()
+//        $('#alert-show1').css('top',15);
+//        $('#alert-show1').show()
+//        shake()
+//    }
+//}
 
-function shake(){
-    setInterval(function(){ 
-        $(".alert-card").hide();
-    }, 3000);
-}
+//function shake(){
+//    setInterval(function(){ 
+//        $(".alert-card").hide();
+//    }, 3000);
+//}
 
 
 
