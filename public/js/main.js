f7ffc1c87b52ac0ce5b616047a5a1b2ba7d0a086
蔡仲庭 <ting@caizhongtingde-MacBook-Pro.local>
2019-03-16 23:16:35 +0800

    version5

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 // Turn on Bootstrap
 $('.sticky').hide()
+
+
 // 15 days from now!
 function get15dayFromNow() {
   return new Date(new Date().valueOf() + 60 * 1160);
@@ -8,6 +10,7 @@ function get15dayFromNow() {
 var $clock = $('.sticky');
 
 
+$('#alert-show1').hide()
 
 
 
@@ -15,7 +18,9 @@ $('#go').click(function() {
   //$clock.countdown(get15dayFromNow());
   $('.sticky').show()
   $('.page1').hide()
-  document.getElementById('surveycake').innerHTML = "<iframe width='100%' height='800' src='https://www.surveycake.com/s/NeQgy' style='overflow:hidden;border:#ddd 1px solid;' allowTransparency='true' frameborder='0'> </iframe>"
+  document.getElementById('surveycake').innerHTML = "<iframe width='100%' height='800' src='https://www.surveycake.com/s/11WXl' style='overflow:hidden;border:#ddd 1px solid;' allowTransparency='true' frameborder='0'> </iframe>"
+  $('.banner').hide()
+  $('.intro').hide()
   setTimeout(function(){
 
     $clock.countdown(get15dayFromNow(), function(event) {
@@ -32,39 +37,49 @@ $('#go').click(function() {
    }, 5000);
 });
 
-$('#alert-show1').hide()
-$('#alert-show2').hide()
+document.getElementById('blink').innerText=""
 
 
 function alert_reminder(minutes , seconds){
     if (minutes==1 && seconds==0){
-      
-        
         //$('#alert-show').show()
         //$('#alert-show1').css('top',15);
+        document.getElementById('blink').innerHTML = "<p class='rr'>剩下60秒快快快快</p>"
         $('#alert-show1').show()
         shake()
     }
-    if (minutes==0 && seconds==55){
-      console.log('sss')
-      
-        
-      //$('#alert-show').show()
-      //$('#alert-show1').css('top',15);
-      $('#alert-show2').show()
-      shake1()
-  }
+    if (minutes==0 && seconds==50){
+      document.getElementById('blink').innerHTML = "<p class='rr'>剩下50秒快快快快</p>"
+      $('#alert-show1').show()
+      shake()
+    }
+    if (minutes==0 && seconds==40){
+      document.getElementById('blink').innerHTML = "<p class='rr'>剩下40秒快快快快</p>"
+      $('#alert-show1').show()
+      shake()
+    }
+    if (minutes==0 && seconds==30){
+      document.getElementById('blink').innerHTML = "<p class='rr'>剩下30秒快快快快</p>"
+      $('#alert-show1').show()
+      shake()
+    }
+    if (minutes==0 && seconds==20){
+      document.getElementById('blink').innerHTML = "<p class='rr'>剩下20秒快快快快</p>"
+      $('#alert-show1').show()
+      shake()
+    }
+    if (minutes==0 && seconds==10){
+      document.getElementById('blink').innerHTML = "<p class='rr'>沒有時間了！快交出問卷！</p>"
+      $('#alert-show1').show()
+      $('.sticky').hide()      
+    }
 
 }
 
 function shake(){
-    setInterval(function(){ 
-        $(".alert-card").hide();
-    }, 3000);
+    setTimeout(function(){
+      $("#alert-show1").hide();
+    } , 3000);
 }
-function shake1(){
-  setInterval(function(){ 
-      $(".alert-card1").hide();
-  }, 3000);
-}
+
 
