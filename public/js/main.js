b2074247fff2265b88845c142b023a4c6022f3e7
蔡仲庭 <caizhongting@caizhongtingde-MacBook-Pro-2.local>
2019-03-25 23:33:09 +0900

    renew main

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@
 
 $('#go').click(function() {
   //$clock.countdown(get15dayFromNow());
-  var items = Array('http://140.114.89.223:3001/tmp' ,'http://140.114.89.223:3001/tmp1', '0');
+  var items = Array('http://140.114.89.223:3001/tmp' ,'http://140.114.89.223:3001/tmp1');
   var item = items[Math.floor(Math.random()*items.length)];
   if (item=='http://140.114.89.223:3001/tmp'){
     window.location.href = "http://140.114.89.223:3001/tmp"
@@ -11,12 +11,12 @@ $('#go').click(function() {
     window.location.href = "http://140.114.89.223:3001/tmp1"
   }
 
-  var pages = Array('https://www.surveycake.com/s/8aNgP' ,'https://www.surveycake.com/s/yo9V0', 'https://www.surveycake.com/s/l03OO','https://www.surveycake.com/s/GRLON');
-  var page = pages[Math.floor(Math.random()*pages.length)];
+  //var pages = Array('https://www.surveycake.com/s/8aNgP' ,'https://www.surveycake.com/s/yo9V0', 'https://www.surveycake.com/s/l03OO','https://www.surveycake.com/s/GRLON');
+  //var page = pages[Math.floor(Math.random()*pages.length)];
 
-  $('.sticky').show()
-  $('.page1').hide()
-  document.getElementById('surveycake').innerHTML = "<iframe width='100%' height='800' src='"+page+"'style='overflow:hidden;border:#ddd 1px solid;' allowTransparency='true' frameborder='0'> </iframe>"
+  //$('.sticky').show()
+  //$('.page1').hide()
+  //document.getElementById('surveycake').innerHTML = "<iframe width='100%' height='800' src='"+page+"'style='overflow:hidden;border:#ddd 1px solid;' allowTransparency='true' frameborder='0'> </iframe>"
   
 });
 
